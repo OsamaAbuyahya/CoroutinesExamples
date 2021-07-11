d3bb6329d56d876e6a2daff37c464598f996b89d
osama1996 <osama@bemotioninc.com>
2021-07-11 16:30:29 +0300

    Async/Await

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,19 +18,43 @@ class MainActivity : AppCompatActivity() {
         txtLabel = findViewById(R.id.txt_label)
 
         // suspend function call from another suspend or coroutines builder
-        runBlocking {
-            printMyTextAfterDelay("Osama Abu Yahya")
-        }
+//        runBlocking {
+//            printMyTextAfterDelay("Osama Abu Yahya")
+//        }
 
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+
+                val dataUser  = async { getUserFromNetwork() }
+                val localUser = async { getUserFromDatabase() }
+
+                if (dataUser.await() == localUser.await()) {
+                    Log.d("Here", "Equals")
+                } else {
+                    Log.d("Here", "Not Equals")
+                }
+            }
+            Log.d("Here", time.toString())
+        }
     }
 
-    suspend fun printMyTextAfterDelay(myText: String){
+    suspend fun printMyTextAfterDelay(myText: String) {
         GlobalScope.launch(Dispatchers.Unconfined) {
             delay(2000)
             // to switch background thread
-            withContext(Dispatchers.Main){
+            withContext(Dispatchers.Main) {
                 txtLabel.text = myText
             }
         }
+    }
+
+    private suspend fun getUserFromNetwork(): String {
+        delay(3500)
+        return "Osama Abu Yahya"
+    }
+
+    private suspend fun getUserFromDatabase(): String {
+        delay(2000)
+        return "Osama Abu Yahya"
     }
 }
